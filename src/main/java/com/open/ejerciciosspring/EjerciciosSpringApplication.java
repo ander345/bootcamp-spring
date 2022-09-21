0a9f724b9279eeec51f0526267855e79ce99ce05
Anderson <jcasas@bancodebogota.com.co>
2022-09-20 20:10:04 -0500

    h2 y spring data JPA

---
+++ src/main/java/com/open/ejerciciosspring/EjerciciosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.open.ejerciciosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciosSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjerciciosSpringApplication.class, args);
+    }
+
+}
